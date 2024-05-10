d1dec5c0685064175a13311babb74aa74179e162
Yarik_Shaman <myarik01@gmail.com>
2024-05-10 15:30:38 +0300

    main page almost ready

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Route, Routes} from "react-router-dom";
+import Main from "./Pages/Main/Main";
+
+function App() {
+
+  return (
+      <Routes>
+          <Route path={'/'} element={<Main/>}/>
+      </Routes>
+  )
+}
+
+export default App
