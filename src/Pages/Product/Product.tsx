963e9e87326b2250d4b4c0ed9d965ea2a5c6a928
Yarik_Shaman <myarik01@gmail.com>
2024-05-11 16:48:05 +0300

    main-bug fixing

---
+++ src/Pages/Product/Product.tsx
@@ -0,0 +1,11 @@
+import styles from './Product.module.css'
+import Header from "../../Components/Header/Header";
+
+export default function Product(){
+
+    return(
+        <div className={styles.background}>
+            <Header />
+        </div>
+    )
+}
